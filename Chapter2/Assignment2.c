2f6ca906ea13fecaf33f7db7ff3c900728d008cd
Salvations9158 <136977550+Salvations9158@users.noreply.github.com>
2025-04-01 03:00:21 +0900

    6번

--- Chapter2/Assignment2.c
+++ Chapter2/Assignment2.c
@@ -1,3 +1,9 @@
+/* 파일명: assignment2.c
+* 내용: PA02. 학생의 번호와 학점을 입력받아 출력하는 프로그램을 작성하시오. 학새으이 번호는 정수로, 학점은 실수로 입력
+* 작성자: 전민규
+* 날짜: 2025.3.25
+* 버전: v1.0
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
--- Chapter2/Assignment3.c
+++ Chapter2/Assignment3.c
@@ -1,3 +1,9 @@
+/* 파일명: assignment3.c
+* 내용: PA03. 날짜를 연, 월, 일로 입력받아서 출력하는 프로그램을 작성하시오. 연, 월, 일은 정수로 입력 받는다.
+* 작성자: 전민규
+* 날짜: 2025.3.25
+* 버전: v1.0
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
